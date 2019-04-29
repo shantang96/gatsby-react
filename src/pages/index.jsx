b4327c3c297a272bb59a941fe846324869ba7a4a
Shantanu Gupta <shantang@usc.edu>
2019-04-29 17:30:30 -0500

    fixed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -63,15 +63,9 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-<<<<<<< HEAD
           Hello, <br /> Welcome to my demo.
         </BigTitle>
         <Subtitle>Build your own homepage and display your favorite things!</Subtitle>
-=======
-          Hello, <br /> I'm John Doe.
-        </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
@@ -111,38 +105,19 @@ const Index = () => (
         <AboutHero>
           <Avatar src={avatar} alt="John Doe" />
           <AboutSub>
-<<<<<<< HEAD
             This website was created using Gatsby. You can do only so much with regular HTML/CSS, for everything else you've got to React.
           </AboutSub>
         </AboutHero>
         <AboutDesc>
           Go on now, scroll up and down. Watch the Parallax in action.
-=======
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-<<<<<<< HEAD
             Say <a href="shantang@usc.edu">Hi</a> or find me on other platforms:{' '}
             <a href="https://www.instagram.com/thelionking96/">Instagram</a>
-=======
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
           </ContactText>
         </Inner>
         <Footer>
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -16,19 +16,13 @@ const Hero = ({ children, offset }) => (
   <>
     <Divider speed={0.2} offset={offset}>
       <UpDown>
-<<<<<<< HEAD
         <SVG icon="triangle" hiddenMobile width={48} stroke={colors.orange} left="5%" top="20%" />
         <SVG icon="hexa" width={48} stroke={colors.yellow} left="60%" top="70%" />
         <SVG icon="triangle" width={48} stroke={colors.grey} left="80%" top="20%" />
-=======
-        <SVG icon="triangle" hiddenMobile width={48} stroke={colors.orange} left="10%" top="20%" />
-        <SVG icon="hexa" width={48} stroke={colors.red} left="60%" top="70%" />
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
         <SVG icon="box" width={6} fill={colors['grey-darker']} left="60%" top="15%" />
       </UpDown>
       <UpDownWide>
         <SVG icon="arrowUp" hiddenMobile width={16} fill={colors['blue-dark']} left="80%" top="10%" />
-<<<<<<< HEAD
         <SVG icon="triangle" width={12} stroke={colors['yellow-light']} left="90%" top="50%" />
         <SVG icon="circle" width={16} fill={colors['grey-darker']} left="70%" top="90%" />
         <SVG icon="triangle" width={16} stroke={colors['grey-darkest']} left="30%" top="65%" />
@@ -40,36 +34,18 @@ const Hero = ({ children, offset }) => (
       </UpDownWide>
       <SVG icon="circle" hiddenMobile width={24} fill={colors['grey-darker']} left="5%" top="70%" />
       <SVG icon="circle" width={6} fill={colors['blue-darkest']} left="4%" top="20%" />
-=======
-        <SVG icon="triangle" width={12} stroke={colors.white} left="90%" top="50%" />
-        <SVG icon="circle" width={16} fill={colors['grey-darker']} left="70%" top="90%" />
-        <SVG icon="triangle" width={16} stroke={colors['grey-darkest']} left="30%" top="65%" />
-        <SVG icon="cross" width={16} stroke={colors['pink-light']} left="28%" top="15%" />
-        <SVG icon="circle" width={6} fill={colors['grey-darkest']} left="75%" top="10%" />
-        <SVG icon="upDown" hiddenMobile width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-      </UpDownWide>
-      <SVG icon="circle" hiddenMobile width={24} fill={colors['grey-darker']} left="5%" top="70%" />
-      <SVG icon="circle" width={6} fill={colors['grey-darkest']} left="4%" top="20%" />
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
       <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="50%" top="60%" />
       <SVG icon="upDown" width={8} fill={colors['grey-darkest']} left="95%" top="90%" />
       <SVG icon="upDown" hiddenMobile width={24} fill={colors['grey-darker']} left="40%" top="80%" />
       <SVG icon="triangle" width={8} stroke={colors['grey-darker']} left="25%" top="5%" />
       <SVG icon="circle" width={64} fill={colors.green} left="95%" top="5%" />
-<<<<<<< HEAD
       <SVG icon="box" hiddenMobile width={32} fill={colors.purple} left="5%" top="90%" />
-=======
-      <SVG icon="box" hiddenMobile width={64} fill={colors.purple} left="5%" top="90%" />
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
       <SVG icon="box" width={6} fill={colors['grey-darkest']} left="10%" top="10%" />
       <SVG icon="box" width={12} fill={colors['grey-darkest']} left="40%" top="30%" />
       <SVG icon="hexa" width={16} stroke={colors['grey-darker']} left="10%" top="50%" />
       <SVG icon="hexa" width={8} stroke={colors['grey-darker']} left="80%" top="70%" />
-<<<<<<< HEAD
       <SVG icon="circle" width={16} fill={colors.green} left="20%" top="97%" />
 
-=======
->>>>>>> 1f7c5dad7daeae9a807e3d9c7f9b5c5e0eb6d0b3
     </Divider>
     <Content speed={0.4} offset={offset}>
       <Wrapper>{children}</Wrapper>
